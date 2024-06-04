68f5fea7929d24b60beaefc08c13043add624403
KingAbhi6848 <sabhi6848@gmail.com>
2024-06-04 18:45:04 +0530

    Added logger Middleware

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import userRouter from './src/features/user/user.routes.js';
 import jwtAuth from './src/middlewares/jwt.middleware.js';
 import cartRouter from './src/features/cartItems/cartItems.routes.js';
 import apiDocs from './swagger.json' assert {type:'json'};
+import loggerMiddleware from './src/middlewares/logger.middlware.js';
 
 // 2. Create Server
 const server = express();
@@ -13,9 +14,22 @@ const server = express();
 server.use(express.json());
 
 
+// cors policy
+
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Allow any origin
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Allow specific HTTP methods
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allow specific headers
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200); // Handle preflight request
+  } else {
+    next();
+  }
+});
 server.use('/api-docs',swagger.serve , swagger.setup(apiDocs))
 // for all requests related to product, redirect to product routes.
 // localhost:3200/api/productss
+server.use(loggerMiddleware);
 server.use(
   '/api/products',
   jwtAuth,
@@ -23,6 +37,8 @@ server.use(
 );
 server.use("/api/cartItems", jwtAuth, cartRouter);
 server.use('/api/users', userRouter);
+
+
 
 // 3. Default request handler
 server.get('/', (req, res) => {
